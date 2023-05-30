7e5c8e0e4a0e708cce7fcfdfbbebee6d111f6436
toritomodachi <tanncheonn@gmail.com>
2023-05-30 19:30:42 +0900

    아 진짜 모르겠네

--- server.js
+++ server.js
@@ -2,7 +2,13 @@ const MongoClient = require('mongodb').MongoClient;
 const express = require('express')
 const app = express()
 const bodyParser= require('body-parser')
-app.use(bodyParser.urlencoded({extended: true}))
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const session = require('express-session');
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(session({secret : '비밀코드', resave : true, saveUninitialized: false}));
+app.use(passport.initialize());
+app.use(passport.session()); 
 app.set('view engine', 'ejs');
 
 var db;
@@ -14,13 +20,9 @@ MongoClient.connect('mongodb+srv://duram4814:aYj6kDFz9kKh8YbS@onelife.4mdp0pa.mo
 		console.log('listening on 7777')
 	});
 });
-const passport = require('passport');
-const LocalStrategy = require('passport-local').Strategy;
-const session = require('express-session');
 
-app.use(session({secret : '비밀코드', resave : true, saveUninitialized: false}));
-app.use(passport.initialize());
-app.use(passport.session()); 
+
+
 app.get('/', function(요청, 응답) { 
   응답.sendFile(__dirname + '/index.html');
 })
@@ -38,17 +40,16 @@ app.post('/add', passport.authenticate('local', {failureRedirect : '/checkfail'}
 
 
 passport.use(new LocalStrategy({
-  usernameField: 'id',
-  passwordField: 'pw',
+  usernameField: 'STUDENT_CODE',
+  passwordField: 'CHECK_CODE',
   session: true,
   passReqToCallback: false,
-}, function (입력한아이디, 입력한비번, done) {
-  //console.log(입력한아이디, 입력한비번);
-  db.collection('login').findOne({ id: 입력한아이디 }, function (에러, 결과) {
+}, function (입력한학번, 입력한출석코드, done) {
+  db.collection('sw_student_profile').findOne({ STUDENT_CODE: 입력한학번 }, function (에러, 결과) {
     if (에러) return done(에러)
 
     if (!결과) return done(null, false, { message: '존재하지않는 학번이요' })
-    if (입력한비번 == 결과.pw) {
+    if (입력한출석코드 == 결과.CHECK_CODE) {
       return done(null, 결과)
     } else {
       return done(null, false, { message: '출석코드 맞게 한거야 ?' })
@@ -57,9 +58,27 @@ passport.use(new LocalStrategy({
 }));
 
 passport.serializeUser(function (user, done) {
-  done(null, user.id)
+  done(null, user.STUDENT_CODE)
 });
 
 passport.deserializeUser(function (아이디, done) {
-  done(null, {})
-});   
+  db.collection('sw_student_profile').findOne({ STUDENT_CODE: 아이디 }, function (에러, 결과) {
+    done(null, 결과)
+  })
+}); 
+
+
+
+app.get('/mypage', 로그인했니, function (요청, 응답) { 
+  console.log(요청.user); 
+  응답.render('mypage.ejs', {사용자: 요청.user}) 
+}) 
+
+function 로그인했니(요청, 응답, next) { 
+  if (요청.user) { 
+    next() 
+  } 
+  else { 
+    응답.send('로그인안하셨는데요?') 
+  } 
+} 
