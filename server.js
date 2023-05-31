e39f15f53d949e6d862a1e6c349ab9a5f7ffdeb4
toritomodachi <tanncheonn@gmail.com>
2023-05-31 19:53:04 +0900

    일단 다 만듬

--- server.js
+++ server.js
@@ -35,16 +35,25 @@ app.get('/checkfail',function(요청,응답){
   응답.render('index3.ejs');
 })
 
+// app.get('/list', function(요청, 응답){
+//   db.collection('sw_student_profile').find().toArray(function(에러, 결과){
+//     console.log(결과)
+//     응답.render('list.ejs', { posts : 결과 })
+//   })
+// })
 
-
+app.post('/add', passport.authenticate('local', {failureRedirect : '/checkfail'}), function(요청, 응답){
+  응답.render('mypage.ejs');
+});
 
 passport.use(new LocalStrategy({
-  usernameField: 'STUDENT_CODE',
-  passwordField: 'CHECK_CODE',
-  session: false,
+  usernameField: 'sc',
+  passwordField: 'cc',
+  session: true,
   passReqToCallback: false,
 }, function (입력한학번, 입력한출석코드, done) {
-  db.collection('sw_student_profile').findOne({ STUDENT_CODE: 입력한학번 }, function (에러, 결과) {
+  console.log(입력한학번, 입력한출석코드);
+  db.collection('sw_student_profile').findOne({ STUDENT_CODE : 입력한학번 }, function (에러, 결과) {
     if (에러) return done(에러);
 
     if (!결과) return done(null, false, { message: '존재하지않는 학번이요' });
@@ -55,34 +64,32 @@ passport.use(new LocalStrategy({
     }
   })
 }));
-// passport.serializeUser(function (user, done) {
-//   done(null, user.STUDENT_CODE);
-// });
 
-// passport.deserializeUser(function (아이디, done) {
-//   db.collection('sw_student_profile').findOne({ STUDENT_CODE: 아이디 }, function (에러, 결과) {
-//     done(null, 결과);
-//   })
-// }); 
-
-app.post('/add', passport.authenticate('local', {failureRedirect : '/checkfail'}), function(요청, 응답){
-  응답.render('mypage.ejs');
+passport.serializeUser(function (user, done) {
+  done(null, user.STUDENT_CODE);
 });
 
-
-
-
+passport.deserializeUser(function (아이디, done) {
+  db.collection('sw_student_profile').findOne({ STUDENT_CODE: 아이디 }, function (에러, 결과) {
+    done(null, 결과);
+  })
+}); 
 
 app.get('/mypage', 로그인했니, function (요청, 응답) { 
   console.log(요청.user); 
-  응답.render('mypage.ejs', {사용자: 요청.user}) ;
+  응답.render('mypage.ejs', {}) 
 }) 
 
 function 로그인했니(요청, 응답, next) { 
   if (요청.user) { 
-    next();
+    next() 
   } 
   else { 
-    응답.send('로그인안하셨는데요?') ;
+    응답.send('로그인안하셨는데요?') 
   } 
 } 
+
+app.get('/mypage', 로그인했니, function (요청, 응답) {
+  console.log(요청.user);
+  응답.render('mypage.ejs', { 사용자: 요청.user })
+}) 
