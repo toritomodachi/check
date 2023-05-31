fc8958139d33efd9aaaf0ab1daf043ece3d264e1
toritomodachi <tanncheonn@gmail.com>
2023-05-31 16:04:38 +0900

    데이터 자료만 잘 가져오면 된다

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const MongoClient = require('mongodb').MongoClient;
-const express = require('express')
-const app = express()
-const bodyParser= require('body-parser')
+const express = require('express');
+const app = express();
+const bodyParser= require('body-parser');
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
 const session = require('express-session');
@@ -10,14 +10,15 @@ app.use(session({secret : '비밀코드', resave : true, saveUninitialized: fals
 app.use(passport.initialize());
 app.use(passport.session()); 
 app.set('view engine', 'ejs');
-
+app.use(express.static('views'));
 var db;
+
 MongoClient.connect('mongodb+srv://duram4814:aYj6kDFz9kKh8YbS@onelife.4mdp0pa.mongodb.net/?retryWrites=true&w=majority', { useUnifiedTopology: true }, function (에러, client) {
 	if (에러) return console.log(에러)
 	db = client.db('bdar_sw_db');
 
 	app.listen(7777, function () {
-		console.log('listening on 7777')
+		console.log('listening on 7777');
 	});
 });
 
@@ -34,51 +35,54 @@ app.get('/checkfail',function(요청,응답){
   응답.render('index3.ejs');
 })
 
-app.post('/add', passport.authenticate('local', {failureRedirect : '/checkfail'}), function(요청, 응답){
-  응답.render('mypage.ejs')
-});
+
 
 
 passport.use(new LocalStrategy({
   usernameField: 'STUDENT_CODE',
   passwordField: 'CHECK_CODE',
-  session: true,
+  session: false,
   passReqToCallback: false,
 }, function (입력한학번, 입력한출석코드, done) {
   db.collection('sw_student_profile').findOne({ STUDENT_CODE: 입력한학번 }, function (에러, 결과) {
-    if (에러) return done(에러)
+    if (에러) return done(에러);
 
-    if (!결과) return done(null, false, { message: '존재하지않는 학번이요' })
+    if (!결과) return done(null, false, { message: '존재하지않는 학번이요' });
     if (입력한출석코드 == 결과.CHECK_CODE) {
-      return done(null, 결과)
+      return done(null, 결과);
     } else {
-      return done(null, false, { message: '출석코드 맞게 한거야 ?' })
+      return done(null, false, { message: '출석코드 맞게 한거야 ?' });
     }
   })
 }));
+// passport.serializeUser(function (user, done) {
+//   done(null, user.STUDENT_CODE);
+// });
 
-passport.serializeUser(function (user, done) {
-  done(null, user.STUDENT_CODE)
+// passport.deserializeUser(function (아이디, done) {
+//   db.collection('sw_student_profile').findOne({ STUDENT_CODE: 아이디 }, function (에러, 결과) {
+//     done(null, 결과);
+//   })
+// }); 
+
+app.post('/add', passport.authenticate('local', {failureRedirect : '/checkfail'}), function(요청, 응답){
+  응답.render('mypage.ejs');
 });
 
-passport.deserializeUser(function (아이디, done) {
-  db.collection('sw_student_profile').findOne({ STUDENT_CODE: 아이디 }, function (에러, 결과) {
-    done(null, 결과)
-  })
-}); 
+
 
 
 
 app.get('/mypage', 로그인했니, function (요청, 응답) { 
   console.log(요청.user); 
-  응답.render('mypage.ejs', {사용자: 요청.user}) 
+  응답.render('mypage.ejs', {사용자: 요청.user}) ;
 }) 
 
 function 로그인했니(요청, 응답, next) { 
   if (요청.user) { 
-    next() 
+    next();
   } 
   else { 
-    응답.send('로그인안하셨는데요?') 
+    응답.send('로그인안하셨는데요?') ;
   } 
 } 
