689bb1bbd0fab2a93ca5e988463ecb2f48129737
toritomodachi <tanncheonn@gmail.com>
2023-06-02 19:47:56 +0900

    아아아아아 오류낫어

--- server.js
+++ server.js
@@ -45,13 +45,13 @@ app.get('/loding',function(요청,응답){
 //   })
 // })
 
-app.put('/add', function(요청, 응답){ 
-  db.collection('sw_student_profile').updateOne( {_id : parseInt(요청.body.id) }, {$set : { COUNT : parseInt(요청.body.COUNT) + 1 }}, 
-    function(){ 
+// app.put('/add', function(요청, 응답){ 
+//   db.collection('sw_student_profile').updateOne( {_id : parseInt(요청.body.id) }, {$set : { COUNT : parseInt(요청.body.COUNT) + 1 }}, 
+//     function(){ 
     
-    console.log('수정완료')
-  }); 
-}); 
+//     console.log('수정완료')
+//   }); 
+// }); 
 
 app.post('/add', passport.authenticate('local', {failureRedirect : '/checkfail'}), function(요청, 응답){
   응답.render('index4.ejs');
@@ -82,23 +82,25 @@ passport.serializeUser(function (user, done) {
 });
 
 passport.deserializeUser(function (아이디, done) {
-  db.collection('sw_student_profile').findOne({ sc : 아이디 }, function (에러, 결과) {
+  db.collection('sw_student_profile').findOne({ id : 아이디 }, function (에러, 결과) {
     done(null, 결과);
   })
 }); 
 
-app.get('/mypage', 로그인했니, function (요청, 응답) { 
-  console.log(요청.user); 
-  응답.render('mypage.ejs', { 사용자: 요청.user }) 
+// app.get('/mypage', 로그인했니, function (요청, 응답) { 
+//   console.log(요청.user); 
+//   응답.render('mypage.ejs', {사용자: 요청.user}) 
+// }) 
+
+// function 로그인했니(요청, 응답, next) { 
+//   if (요청.user) {
+//     next()
+//   } 
+//   else { 
+//     응답.send('로그인안하셨는데요?') 
+//   } 
+// } 
+
+app.get('/mypage', function (요청, 응답) {
+  응답.render('mypage.ejs', {})
 }) 
-
-function 로그인했니(요청, 응답, next) { 
-  if (요청.user) { 
-    next() 
-  } 
-  else { 
-    응답.send('로그인안하셨는데요?') 
-  } 
-} 
-
-
